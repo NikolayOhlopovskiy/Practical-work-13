ae975f349cab195c3cde76ae8deb504f2b247eec
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2024-11-26 14:14:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13._1
+{
+    /// <summary>
+    /// //Задача 1.Найти минимальную величину из двух целых переменных a, b,
+    //используя вспомогательные методы
+    //(ввода аргументов, нахождения минимальной величины, вывода результата).
+    /// </summary>
+    class Program
+    {
+        static int min(int a,int b)
+        {
+            return a < b ? a : b;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{min(a, b)}");
+            Console.Read();
+        }
+    }
+}
